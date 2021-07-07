7f0cffea3d76a54f7bf6f8033f5f34557b213cd5
Reilley Ford <reilleyford@gmail.com>
2021-07-07 12:56:01 -0400

    Moved class files to src

---
+++ src/walletScout.py
@@ -0,0 +1,41 @@
+import json
+from wallet import Wallet
+
+# Main class for program.
+# Requires JSON data of some kind during instantiation
+class WalletScout:
+    def __init__(self, jData):
+        self._jData   = jData
+        self._dsData  = self.deserializeData()
+        self._wallets = []
+
+    def deserializeData(self):
+        self._dsData = json.load(self._jData)
+        return self._dsData
+
+    def __loadWallets(self, dsData):
+        for key, value in dsData.items():
+            if isinstance(value, dict):
+                self.__loadWallets(value)
+            else:
+                assets = value[0].get('assets')
+                if isinstance(assets, list):
+                    for item in assets:
+                        asset = Wallet(item.get('type'),
+                                       item.get('category'),
+                                       item.get('address'),
+                                       item.get('symbol'),
+                                       item.get('decimals'),
+                                       item.get('label'),
+                                       item.get('img'),
+                                       item.get('hide'),
+                                       item.get('canExchange'),
+                                       item.get('price'),
+                                       item.get('balance'),
+                                       item.get('balanceRaw'),
+                                       item.get('balanceUSD'))
+                        self._wallets.append(asset)
+
+    def getWallets(self):
+        self.__loadWallets(self._dsData)
+        return self._wallets
