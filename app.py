fc2f59b27fa73cb8232cfa6c0379a160290bcb4d
Reilley Ford <reilleyford@gmail.com>
2021-07-07 16:07:47 -0400

    Added more usage examples and comments

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import sys, json
-from wallet import Wallet
-from walletScout import WalletScout
+from Wallets import Wallet, WalletScout
 
 # Mostly for testing, using cmd line argv for file I/O
 # ex. Call from cmd: python app.py Test.json
@@ -11,10 +10,24 @@ if len(sys.argv) > 1:
 else:
     file = input("No file found. Enter path:")
 
+# The only purpose of app.py is for testing both classes
+# and as usage examples.
 walletScout = WalletScout(file)
 
-wallets = walletScout.getWallets()
+# Iterate _wallets list attr
+# print each address and balance in USD
+for wallet in walletScout._wallets:
+    print('Address: ', wallet._address)
+    print('Balance (USD): ', wallet._balanceUSD)
 
+# Iterates _wallets list attr
+# Sums balance in USD
+totalBalance = 0
+for wallet in walletScout._wallets:
+    totalBalance += wallet._balanceUSD
+print('Total Balance (USD)', totalBalance)
+
+# Simple printout of each wallet object and all attributes.
 for item in wallets:
     print("WALLET OBJECT")
     print(item._type)
