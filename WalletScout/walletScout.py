6a1ffb30cfcc1dfdec928078014dab10a0f29c7f
Reilley Ford <reilleyford@gmail.com>
2021-07-09 13:25:07 -0400

    Validation of JSON. More mixed obj passing support

--- WalletScout/walletScout.py
+++ WalletScout/walletScout.py
@@ -1,4 +1,5 @@
-import json
+import json, os.path
+from os import path
 # Wallet class, auto generated from a passed dictionary.
 # It is being used to build objects from a list of dicts.
 
@@ -21,14 +22,24 @@ class Wallet:
 # URL / JSON String dual functionality
 class WalletScout:
     def __init__(self, jData):
-        self._jData   = jData
+        if isinstance(jData, str) and path.exists(jData):
+            self._jData = open(jData)
+        else:
+            self._jData = jData
+
         self._dsData  = self.__deserializeData()
         self._wallets = []
-        self._
         self.__loadWallets(self._dsData)
 
     def __deserializeData(self):
-        self._dsData = json.load(self._jData)
+        if isinstance(self._jData, str):
+            try:
+                self._dsData = json.loads(self._jData)
+            except json.decoder.JSONDecodeError as e:
+                print("String object does not meet JSON formatting specifications. Error: ", e)
+                return
+        else:
+            self._dsData = json.load(self._jData)
         return self._dsData
 
     def __loadWallets(self, dsData):
