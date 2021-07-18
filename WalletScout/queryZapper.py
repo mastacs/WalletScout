43878dc30c6fff4847748ff176719a2d0fba92d4
mastacs <54516840+mastacs@users.noreply.github.com>
2021-07-18 19:03:14 -0400

    Update queryZapper.py

--- WalletScout/queryZapper.py
+++ WalletScout/queryZapper.py
@@ -1,30 +1,64 @@
+from typing import Protocol
 from walletScout import Scout
 import requests
 
 # Zapper API call
-eth_url = "https://api.zapper.fi/v1/protocols/tokens/balances?addresses[]=%s&network=%s&api_key=96e0cc51-a62e-42ca-acee-910ea7d2a241"
-testAddress = "0x41146f3acB162FA19daC0cdC54C495a45c8aFefF"
+walletBalance_url = "https://api.zapper.fi/v1/protocols/tokens/balances?addresses[]=%s&network=%s&api_key=96e0cc51-a62e-42ca-acee-910ea7d2a241"
+protocolBalance_url = "https://api.zapper.fi/v1/protocols/%s/balances?addresses[]=%s&network=%s&api_key=96e0cc51-a62e-42ca-acee-910ea7d2a241"
+testAddress = "0x01e6fd0aE73D9194b19f9B376065577927A0D5f5"
 
-# Add network supported by API here
-networks = [
-   "ethereum"
+protocols = [
+   "aave", "aave-amm", "aave-v2", "adamant", "alchemix", "alpha", "augur", "autofarm", "b-protocol", "badger", "balancer", "bancor", "barnbridge", "beefy", "bella", "belt", "bitcoin", "bzx", "compound", "convex", "cover", "cream", "curve", "defisaver", "defi-swap", "derivadex", "deversifi","dforce","dhedge", "dodo", "dsd", "dydx", "88mph", "ellipsis", "epns", "esd", "futureswap","harvest", "hegic", "idle", "inverse", "keeper-dao", "kyber-dmm", "linkswap", "liquity", "loopring", "maker", "mintr", "mooniswap", "mushroom", "nft", "1inch", "opyn", "other", "pancakeswap", "pickle", "pooltogether", "quickswap", "rari", "realt", "reflexer", "ribbon", "sablier", "saddle", "sfinance", "shell", "smoothy", "snowswap", "spookyswap", "sushiswap", "swerve", "synthetix", "the-graph", "tokensets", "tokens", "uniswap", "uniswap-v2", "uniswap-v3", "unit", "value", "venus", "vesper", "waultswap", "xsigma", "yaxis", "yearn"
 ]
 
-for network in networks:
-   # Make the GET request
-   response = requests.get(url = (eth_url % (testAddress.rstrip(), network)))
+networks = [
+   "ethereum", "fantom"
+]
+
+def checkWalletsBalance(address, network):
+   _totalBalanceUSD = 0
+   response = requests.get(url = (
+      walletBalance_url % (
+         address,
+         network
+      )
+   ))
    # Instantiate the Wallet object passing response as JSON object
    zapper = Scout(response.json(), network)
+   #Check Balance in Wallet 
+   for token in zapper._network[network]:
+      _totalBalanceUSD += token.balanceUSD
+      print('Balance Found in Contract Address: ' + token.address)
+   return _totalBalanceUSD
 
-   # Testing
-   totalUSD = 0
-   
-   # Ex. Check Ethereum Network for balance
-   for token in zapper._network["ethereum"]:
-      totalUSD += token.balanceUSD
-      print('Address: ' + token.address)
+def checkBalancePerProtocol(address, network):
+   _totalBalanceUSD = 0
 
-   # Check Fantom Network, etc
+   for protocol in protocols:
+      response = requests.get(url = (
+         protocolBalance_url % (
+            protocol,
+            address,
+            network
+         )
+      ))
+      # Instantiate the Wallet object passing response as JSON object
+      zapper = Scout(response.json(), network)
+      #Check Balance in Wallet 
+      for protocolBalance in zapper._network[network]:
+         _totalBalanceUSD += protocolBalance.balanceUSD
+         print('Protocol Address: ' + protocolBalance.address)
+         print(_totalBalanceUSD)
+   return _totalBalanceUSD
 
-   # Testing
-   print('Total Balance (USD): ', totalUSD)
+
+def main():
+   for network in networks:
+      #Check Wallets Balance in Protocols
+      walletBalance = checkWalletsBalance(testAddress, network)
+      protocolBalance = checkBalancePerProtocol(testAddress, network)
+      # (Testing)
+      totalUSD = walletBalance + protocolBalance
+      print('Total Balance (USD): ', totalUSD)
+
+main()
