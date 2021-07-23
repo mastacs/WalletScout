68b905fafb9ab28e892186427915603dcea6dbfe
mastacs <54516840+mastacs@users.noreply.github.com>
2021-07-23 07:27:57 -0400

    Update queryEtherscan.py
    refractor

--- WalletScout/queryEtherscan.py
+++ WalletScout/queryEtherscan.py
@@ -1,6 +1,6 @@
 import requests
 from os import path
-from zapperScout import Scout
+from walletScout import Scout
 from zapperVariables import testAddress
 
 etherscan_url = "https://api.etherscan.io/api?module=account&action=%s&address=%s&startblock=0&endblock=999999999&sort=asc&apikey=M4SIQFEQ5D6KXNQMIZXF86MX2R8XAK5NDJ"
@@ -13,55 +13,36 @@ searchType = {
 
 class EtherScanQuery:
    def __init__(self, address):
-      self._etherscan = {}
-      self.__getTx(address)
-      self.__getERC20Events(address)
+      self._etherscanData = {}
+      self.__getData(address)
    
-   def __getTx(self, address):
-      self._internalTx = []
-      self._externalTx = []
-      
-      externalTxResponse = requests.get(url = (
-         etherscan_url % (
-         searchType["externalTx"],
-         address
+   def __getData(self, address):
+       self._internalTx = []
+       self._externalTx = []
+       self._tokenEvents = []
+       for key, value in searchType.items():
+            response = requests.get(url = (
+                etherscan_url % (
+                    searchType[key],
+                    address
          )
       ))
-      internalTxResponse = requests.get(url = (
-         etherscan_url % (
-         searchType["internalTx"],
-         address
-         )
-      ))
-
-      results = Scout(externalTxResponse.json())
-      for result in results._assets:
-         self._externalTx.append(result)
-
-      results = Scout(internalTxResponse.json())
-      for result in results._assets:
-         self._internalTx.append(result)
-
-      self._etherscan["external"] = self._externalTx
-      self._etherscan["internal"] = self._internalTx
-
-   def __getERC20Events(self, address):
-      self._tokenEvents = []
-      response = requests.get(url = (
-         etherscan_url % (
-         searchType["tokenEvents"],
-         address
-         )
-      ))
-      results = Scout(response.json())
-      for result in results._assets:
-         self._tokenEvents.append(result)
-      self._etherscan["tokens"] = self._tokenEvents
+            results = Scout(response.json())
+            for result in results._assets:
+                if key == 'externalTx':
+                    self._externalTx.append(result)
+                elif key == 'interalTx':
+                    self._internalTx.append(result)
+                elif key == 'tokenEvents':
+                    self._tokenEvents.append(result)
+            self._etherscanData["external"] = self._externalTx
+            self._etherscanData["internal"] = self._internalTx
+            self._etherscanData["tokenEvents"] = self._tokenEvents
 
 def main():
    results = EtherScanQuery(testAddress)
-   for internal in results._etherscan["internal"]:
+   for internal in results._etherscanData["internal"]:
       print(internal.hash)
-   for token in results._etherscan["tokens"]:
+   for token in results._etherscanData["tokenEvents"]:
       print(token.hash)
 main()
